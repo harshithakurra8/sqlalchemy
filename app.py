fdb518c77918fe8dc5157660adb2b60719235986
harshithakurra8 <harshitha9.kr@gmail.com>
2024-07-04 11:12:21 +0530

    sqlalchemy

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+from sqlalchemy.orm import sessionmaker
 
+from models import user,engine
+
+Session=sessionmaker(bind=engine)
+session=Session()
+user=User(name="Harry",age=30)
+
+session.add(user)
+
+session.commit()
